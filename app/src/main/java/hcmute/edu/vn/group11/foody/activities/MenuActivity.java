64a67b29be9373c7ac41b9be495c71767b874984
hiepvu0201 <43855950+hiepvu0201@users.noreply.github.com>
2020-07-01 02:06:24 +0700

    show all food and food list

--- app/src/main/java/hcmute/edu/vn/group11/foody/activities/MenuActivity.java
+++ app/src/main/java/hcmute/edu/vn/group11/foody/activities/MenuActivity.java
@@ -1,13 +1,17 @@
 package hcmute.edu.vn.group11.foody.activities;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import hcmute.edu.vn.group11.foody.CustomExpandableListView;
 import hcmute.edu.vn.group11.foody.R;
@@ -18,6 +22,8 @@ public class MenuActivity extends AppCompatActivity {
     List<String> listdataHeader;
     HashMap<String,List<String>> listdataChild;
     CustomExpandableListView customExpandableListView;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +34,23 @@ public class MenuActivity extends AppCompatActivity {
         expandableListView.setAdapter(customExpandableListView);
     }
 
+   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
    private void addControl() {
 
        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
        listdataHeader = new ArrayList<>();
        listdataChild = new HashMap<String, List<String>>();
 
-       listdataHeader.add("Bò Mỹ Nhúng Ớt");
-       listdataHeader.add("Bún Đậu");
-       listdataHeader.add("Món Thêm");
-
-       List<String> BoMyNhungOt = new ArrayList<String>();
-       BoMyNhungOt.add("Bò Mỹ nhúng ớt nhở");
-       BoMyNhungOt.add("Bò Mỹ nhúng ớt vừa");
-       BoMyNhungOt.add("Bò Mỹ nhúng ớt lớn");
-       BoMyNhungOt.add("Bê thui mẹt nhỏ");
-       BoMyNhungOt.add("Bê thui mẹt vừa");
-       BoMyNhungOt.add("Bê thui mẹt lớn");
-
-       List<String> BunDau = new ArrayList<String>();
-       List<String> MonThem = new ArrayList<String>();
 
 
-       listdataChild.put(listdataHeader.get(0), BoMyNhungOt);
-       listdataChild.put(listdataHeader.get(1), BunDau);
-       listdataChild.put(listdataHeader.get(2), MonThem);
+       listdataHeader.add("Đặc sản");
+       listdataHeader.add("Món mới");
+
+       List<String> dacSan = new ArrayList<String>();
+       Intent intent = getIntent();
+       dacSan.add(Objects.requireNonNull(intent.getExtras()).getString("Name")+" - "+intent.getExtras().getString("Gia"));
+
+       listdataChild.put(listdataHeader.get(0), dacSan);
 
    }
 }
